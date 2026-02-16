ed8eb3211591c68a3b6f6adf944fd076a0355367
Aaron Will Djaba <papiliocurtis@gmail.com>
2026-02-16 16:54:47 +0000

    Update root route to set default page title as "Aaron Will Djaba - Full Stack Developer"

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,6 +14,7 @@ import {AudioProvider} from "@/contexts/audio-context";
 
 export const Route = createRootRoute({
     head: () => ({
+        title: "Aaron Will Djaba - Full Stack Developer",
         links: [
             {
                 rel: 'stylesheet',
